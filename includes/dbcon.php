544ca8e908260ec83c8a964a84696ef079422750
Jos33y <josephlagbalu@gmail.com>
2020-03-06 08:19:18 +0100

    added files for archive mall

---
+++ includes/dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+//$con = mysqli_connect("localhost", "Joseey", "", "eacez-shop");
+
+$con = mysqli_connect("localhost", "u721127448_Shop", "LagbaluJ19", "u721127448_eshop");
+
+//$conn = mysqli_connect("localhost", "Joseey", "", "eacez_db");
+
+$conn = mysqli_connect("localhost", "u721127448_Admin", "LagbaluJ19", "u721127448_eacez");
+
+?>
